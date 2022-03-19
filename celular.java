c467c514fa9b44d109339f39680a10db506d22fd
LNBelen <85247351+LNBelen@users.noreply.github.com>
2022-03-18 21:42:11 -0300

    Create celular.java

---
+++ celular.java
@@ -0,0 +1,19 @@
+package proyecto;
+
+public class celular {
+ 
+       //atributos de la clase
+private String marca;
+private String modelo;
+private String color;
+
+public celular(){
+
+}
+
+// método hacer llamada
+public void llamar(String nombre){
+System.out.println("LLamando a "+nombre);
+}
+
+ 
