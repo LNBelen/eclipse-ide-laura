ebd52e6bfb8d037271db5c673a654e5833ebd2fc
LNBelen <85247351+LNBelen@users.noreply.github.com>
2022-10-07 20:27:55 -0300

    Create db.php
    se crea la conexion con la base de datos

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+
+    $mysql = new mysqli("localhost", "root","", "android users");
+
+    if($mysql->connect_error)
+    {
+
+        die("fallo la conexion");
+    }
+    else
+    {
+
+        echo "conexion satisfactoria";
+    }
+?>
